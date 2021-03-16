f3e26b49ce41f57621c4c2dc384102d0a635ff86
RaulWong12 <daniel.wong@hiumanlab.com>
2021-03-15 21:06:09 -0600

    Updates

---
+++ precache-manifest.1ffd1e47594e73e8ddebf496f4040a74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72a4283d4f69d3982a7224eb0aaa6dd",
+    "url": "/http//jftatjkf.lucusvirtual.es/index.html"
+  },
+  {
+    "revision": "9f8871480d46948efb7b",
+    "url": "/http//jftatjkf.lucusvirtual.es/static/css/2.49e27765.chunk.css"
+  },
+  {
+    "revision": "5d94344f7aff16b6418d",
+    "url": "/http//jftatjkf.lucusvirtual.es/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "9f8871480d46948efb7b",
+    "url": "/http//jftatjkf.lucusvirtual.es/static/js/2.a0f20a53.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/http//jftatjkf.lucusvirtual.es/static/js/2.a0f20a53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d94344f7aff16b6418d",
+    "url": "/http//jftatjkf.lucusvirtual.es/static/js/main.46facd12.chunk.js"
+  },
+  {
+    "revision": "f74e2382f441f5036adb",
+    "url": "/http//jftatjkf.lucusvirtual.es/static/js/runtime-main.5e0c7fb2.js"
+  }
+]);
