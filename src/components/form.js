7d7c5ba41508305b42ac74a92a1db17a60435024
RaulWong12 <daniel.wong@hiumanlab.com>
2021-03-18 14:34:45 -0600

    onboardin v1

--- src/components/form.js
+++ src/components/form.js
@@ -35,7 +35,7 @@ const APIForm  = (props) => {
 
         TOCautocapture('container', {
             locale: "es",
-            session_id: sessionId,
+            session_id: "33e8503a3d604fdebf2b9c818c05a327",
             document_type: dtype,
             document_side: "back",
             http: true,
@@ -64,7 +64,7 @@ const APIForm  = (props) => {
 
         TOCautocapture('containerfront', {
             locale: "es",
-            session_id: sessionId,
+            session_id: "33e8503a3d604fdebf2b9c818c05a327",
             document_type: dtype,
             document_side: "front",
             callback: function(captured_token, image){ 
@@ -91,7 +91,7 @@ const APIForm  = (props) => {
 
         TOCliveness ('liveness', {
         locale: "es",
-        session_id: sessionId,
+        session_id: "33e8503a3d604fdebf2b9c818c05a327",
         http: true,
         callback: function(token){ 
         message.success('Captura Realizada', 3)
