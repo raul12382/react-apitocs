410891018752b1eb96bfdeb5de61748fa0700463
RaulWong12 <daniel.wong@hiumanlab.com>
2021-03-23 11:48:31 -0600

    react

--- src/components/form.js
+++ src/components/form.js
@@ -37,7 +37,7 @@ const APIForm  = (props) => {
 
         TOCautocapture('container', {
             locale: "es",
-            session_id: "717bac8b060a4a0baca7e9d94df8f3c1",
+            session_id: "3eb36b6a60924bcf968bd9a6e7be123c",
             document_type: dtype,
             document_side: "back",
             callback: function(captured_token, image){ 
@@ -66,7 +66,7 @@ const APIForm  = (props) => {
 
         TOCautocapture('containerfront', {
             locale: "es",
-            session_id: "717bac8b060a4a0baca7e9d94df8f3c1",
+            session_id: "3eb36b6a60924bcf968bd9a6e7be123c",
             document_type: dtype,
             document_side: "front",
             callback: function(captured_token, image){ 
@@ -94,7 +94,7 @@ const APIForm  = (props) => {
 
         TOCliveness ('liveness', {
         locale: "es",
-        session_id: "717bac8b060a4a0baca7e9d94df8f3c1",
+        session_id: "3eb36b6a60924bcf968bd9a6e7be123c",
         callback: function(token){ 
         message.success('Captura Realizada', 3)
         const tl= token  
